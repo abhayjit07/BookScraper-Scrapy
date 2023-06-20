171bf85d4b40882bb004c35dd721578abac646a7
Abhayjit Singh Gulati <100589347+abhayjit07@users.noreply.github.com>
2023-06-20 16:48:17 +0530

    Update bookspider.py

--- bookscraper/bookscraper/spiders/bookspider.py
+++ bookscraper/bookscraper/spiders/bookspider.py
@@ -1,16 +1,21 @@
 import scrapy
+import random
 from bookscraper.items import BookItem  
 
 class BookspiderSpider(scrapy.Spider):
     name = 'bookspider'
     allowed_domains = ['books.toscrape.com']
     start_urls = ['https://books.toscrape.com/']
+    custom_settings = {
+        'FEEDS': {
+            'booksdata.json':{'format':'json', 'overwrite':True},
+        }
+    }
 
     def parse(self, response):
         books = response.css('article.product_pod')
         for book in books:
             relative_url = book.css('h3 a ::attr(href)').get()
-
             if 'catalogue/' in relative_url:
                 book_url = 'https://books.toscrape.com/' + relative_url
             else:
@@ -29,9 +34,7 @@ class BookspiderSpider(scrapy.Spider):
     def parse_book_page(self, response):
 
         table_rows = response.css("table tr")
-        book_item = BookItem()
-
-    
+        book_item = BookItem()   
         book_item['url'] = response.url,
         book_item['title'] = response.css('.product_main h1::text').get(),
         book_item['upc'] = table_rows[0].css("td ::text").get()
